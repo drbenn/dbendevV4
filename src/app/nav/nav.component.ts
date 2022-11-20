a6f3300398a06a7101da9b05d8693928fe3896a3
ratlord <bennett.daniel@gmail.com>
2022-11-20 16:27:52 -0500

    nav-burger animated

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  burgerClasses= ['line-1', 'line-2', 'line-3'];
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  setBurgerClass(event:any) {
+    const nodeList = Array(event.target.childNodes)[0];
+
+    nodeList.forEach((nodeItem:any) => {
+      const clss:string = nodeItem.className;
+      const lastChar: string | number = clss[clss.length-1];
+      let burgerIndex:number = Number(clss[5]) - 1;
+
+      if (lastChar !== 'x') {
+        this.burgerClasses[burgerIndex] = this.burgerClasses[burgerIndex] + "-x";
+        }
+        else {
+          this.burgerClasses[burgerIndex] = this.burgerClasses[burgerIndex].substring(0,6);
+        }
+      })
+    }
+  }
+
